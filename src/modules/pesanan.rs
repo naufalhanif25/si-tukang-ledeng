1b511886ebb5d37166a55a05e0de4579c4d3bf96
naufalhanif25 <naufal.hanif2525@gmail.com>
2025-11-24 08:58:33 +0700

    refactor: Refactor Pesanan

--- src/modules/pesanan.rs
+++ src/modules/pesanan.rs
@@ -19,95 +19,32 @@ pub struct Pesanan {
 }
 
 impl Pesanan {
-    pub fn new(id: String, nama: String, tarif: f32, kategori: Kategori, lokasi: String, rekening: i32, rekening_type: MetodePembayaran, jadwal: NaiveDateTime, layanan: Layanan) -> Self {
-        return Self {
-            id: id.clone(),
-            nama,
-            tarif,
-            kategori,
-            lokasi, 
-            rekening,
-            rekening_type: rekening_type.clone(),
-            status: StatusPembayaran::Pending,
-            jadwal,
-            layanan
-        }
+    pub fn new(id: String, nama: &str, tarif: f32, kategori: Kategori, lokasi: &str, rekening: i32, rekening_type: MetodePembayaran, jadwal: NaiveDateTime, layanan: Layanan) -> Self {
+        return Self { id: id.clone(), nama: nama.to_string(), tarif, kategori, lokasi: lokasi.to_string(),  rekening, rekening_type: rekening_type.clone(), status: StatusPembayaran::Pending, jadwal, layanan }
     }
 
-    pub fn set_nama(&mut self, nama: String) {
-        self.nama = nama;
-    }
-
-    pub fn get_nama(&self) -> &str {
-        return &self.nama;
-    }
-
-    pub fn set_tarif(&mut self, tarif: f32) {
-        self.tarif = tarif;
-    }
-
-    pub fn get_tarif(&self) -> &f32 {
-        return  &self.tarif;
-    }
-
-    pub fn set_kategori(&mut self, kategori: Kategori) {
-        self.kategori = kategori;
-    }
-
-    pub fn get_kategori(&self) -> &Kategori {
-        return  &self.kategori;
-    }
-
-    pub fn set_lokasi(&mut self, lokasi: String) {
-        self.lokasi = lokasi;
-    }
-
-    pub fn get_lokasi(&self) -> &str {
-        return  &self.lokasi;
-    }
-
-    pub fn set_rekening(&mut self, rekening: i32) {
-        self.rekening = rekening;
-    }
-
-    pub fn get_rekening(&self) -> &i32 {
-        return  &self.rekening;
-    }
-
-    pub fn set_rekening_type(&mut self, rekening_type: MetodePembayaran) {
-        self.rekening_type = rekening_type;
-    }
-
-    pub fn get_rekening_type(&self) -> &MetodePembayaran {
-        return  &self.rekening_type;
-    }
-
-    pub fn set_status(&mut self, status: StatusPembayaran) {
-        self.status = status;
-    }
-
-    pub fn get_status(&self) -> &StatusPembayaran {
-        return &self.status;
-    }
-
-    pub fn set_jadwal(&mut self, jadwal: NaiveDateTime) {
-        self.jadwal = jadwal;
-    }
-
+    pub fn set_nama(&mut self, nama: String) { self.nama = nama }
+    pub fn get_nama(&self) -> &str { return &self.nama }
+    pub fn set_tarif(&mut self, tarif: f32) { self.tarif = tarif }
+    pub fn get_tarif(&self) -> &f32 { return  &self.tarif }
+    pub fn set_kategori(&mut self, kategori: Kategori) { self.kategori = kategori }
+    pub fn get_kategori(&self) -> &Kategori { return  &self.kategori }
+    pub fn set_lokasi(&mut self, lokasi: String) { self.lokasi = lokasi }
+    pub fn get_lokasi(&self) -> &str { return  &self.lokasi }
+    pub fn set_rekening(&mut self, rekening: i32) { self.rekening = rekening }
+    pub fn get_rekening(&self) -> &i32 { return  &self.rekening }
+    pub fn set_rekening_type(&mut self, rekening_type: MetodePembayaran) { self.rekening_type = rekening_type }
+    pub fn get_rekening_type(&self) -> &MetodePembayaran { return &self.rekening_type }
+    pub fn set_status(&mut self, status: StatusPembayaran) { self.status = status }
+    pub fn get_status(&self) -> &StatusPembayaran { return &self.status }
+    pub fn set_jadwal(&mut self, jadwal: NaiveDateTime) { self.jadwal = jadwal }
+    
     pub fn set_jadwal_now(&mut self) {
-        let local    = Local::now();
+        let local = Local::now();
         self.jadwal = NaiveDateTime::new(local.date_naive(), local.time());
     }
 
-    pub fn get_jadwal(&self) -> &NaiveDateTime {
-        return &self.jadwal;
-    }
-
-    pub fn set_layanan(&mut self, layanan: Layanan) {
-        self.layanan = layanan;
-    }
-
-    pub fn get_layanan(&self) -> &Layanan {
-        return &self.layanan;
-    }
+    pub fn get_jadwal(&self) -> &NaiveDateTime { return &self.jadwal }
+    pub fn set_layanan(&mut self, layanan: Layanan) { self.layanan = layanan }
+    pub fn get_layanan(&self) -> &Layanan { return &self.layanan }
 }
