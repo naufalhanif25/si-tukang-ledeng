64bba4a8006379f6889c6b5baf34ab37b0f95b67
naufalhanif25 <naufal.hanif2525@gmail.com>
2025-11-24 08:56:34 +0700

    feat: Add new methods

--- src/modules/enums/status_pembayaran.rs
+++ src/modules/enums/status_pembayaran.rs
@@ -1,6 +1,27 @@
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+use strum_macros::EnumIter;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, EnumIter)]
 pub enum StatusPembayaran {
     Berhasil,
     Pending,
     Gagal,
+}
+
+impl StatusPembayaran {
+    pub fn as_string(&self) -> &'static str {
+        match self {
+            StatusPembayaran::Berhasil => "Berhasil",
+            StatusPembayaran::Pending => "Pending",
+            StatusPembayaran::Gagal => "Gagal",
+        }
+    }
+
+    pub fn from_string(input: &str) -> Option<StatusPembayaran> {
+        match input {
+            "Berhasil" => Some(StatusPembayaran::Berhasil),
+            "Pending" => Some(StatusPembayaran::Pending),
+            "Gagal" => Some(StatusPembayaran::Gagal),
+            _ => None,
+        }
+    }
 }
