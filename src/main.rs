2da563787b2f51a32e8752dc4543b857612b7e1e
naufalhanif25 <naufal.hanif2525@gmail.com>
2025-11-25 00:32:11 +0700

    refactor: main function

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,22 @@
 mod modules;
 
 use std::io;
+use crossterm::{ execute, terminal::{ Clear, ClearType } };
 use modules::user::User;
 use modules::tukang_ledeng::TukangLedeng;
 use modules::pesanan::Pesanan;
-use modules::cari_tukang_ledeng::CariTukangLedeng;
+use modules::dashboard;
 use modules::utils;
 
 fn main() {
-    let mut daftar_user: Vec<User> = Vec::new();
-    let mut daftar_tukang_ledeng: Vec<TukangLedeng> = Vec::new();
-    let mut daftar_pesanan: Vec<Pesanan> = Vec::new();
-    let mut opsi = String::new();
+    let mut daftar_user: Vec<User> = utils::load_users_from_file("database/users.json");
+    let mut daftar_tukang_ledeng: Vec<TukangLedeng> = utils::load_tukang_from_file("database/tukang_ledeng.json");
+    let mut daftar_pesanan: Vec<Pesanan> = utils::load_pesanan_from_file("database/pesanan.json");
 
-    println!("Selamat Datang di Sistem Penyewaan Jasa Tukang Ledeng\n");
-    'dashboard_loop: loop {
-        utils::menu_generator("Daftar opsi tersedia", vec!["Masuk", "Daftar", "Keluar"]);
-        println!("Masukkan opsi: ");
-        opsi.clear();
-        io::stdin().read_line(&mut opsi).expect("Gagal membaca input\n");
-        let mut opsi_input: i32 = opsi.trim().parse().expect("Opsi harus angka\n");
-        utils::generate_newline(1);
+    execute!(io::stdout(), Clear(ClearType::All)).unwrap();
+    dashboard::main_menu(&mut daftar_user, &mut daftar_tukang_ledeng, &mut daftar_pesanan);
 
-        if opsi_input == 1 {
-            'auth_loop: loop {
-                utils::menu_generator("Masuk sebagai", vec!["Pengguna", "Tukang Ledeng", "Kembali"]);
-                println!("Masukkan opsi: ");
-                opsi.clear();
-                io::stdin().read_line(&mut opsi).expect("Gagal membaca input\n");
-                opsi_input = opsi.trim().parse().expect("Opsi harus angka\n");
-                utils::generate_newline(1);
-
-                if opsi_input == 1 {
-                    match utils::auth_menu(&mut daftar_user, &mut daftar_tukang_ledeng, &utils::UserRole::User, &utils::AuthType::Login) {
-                        utils::MenuReturn::Lanjut => { continue 'dashboard_loop }
-                        utils::MenuReturn::Kembali => { continue 'auth_loop }
-                    }
-                }
-                else if opsi_input == 2 {
-                    match utils::auth_menu(&mut daftar_user, &mut daftar_tukang_ledeng, &utils::UserRole::Tukang, &utils::AuthType::Login) {
-                        utils::MenuReturn::Lanjut => { continue 'dashboard_loop }
-                        utils::MenuReturn::Kembali => { continue 'auth_loop }
-                    }
-                }
-                else if opsi_input == 3 { continue 'dashboard_loop }
-                else { println!("Opsi '{}' tidak tersedia\n", opsi_input) }
-            }
-        }
-        else if opsi_input == 2 {
-            'auth_loop: loop {
-                utils::menu_generator("Daftar sebagai", vec!["Pengguna", "Tukang Ledeng", "Kembali"]);
-                println!("Masukkan opsi: ");
-                opsi.clear();
-                io::stdin().read_line(&mut opsi).expect("Gagal membaca input\n");
-                opsi_input = opsi.trim().parse().expect("Opsi harus angka\n");
-                utils::generate_newline(1);
-
-                if opsi_input == 1 {
-                    match utils::auth_menu(&mut daftar_user, &mut daftar_tukang_ledeng, &utils::UserRole::User, &utils::AuthType::Register) {
-                        utils::MenuReturn::Lanjut => { continue 'dashboard_loop }
-                        utils::MenuReturn::Kembali => { continue 'auth_loop }
-                    }
-                }
-                else if opsi_input == 2 {
-                    match utils::auth_menu(&mut daftar_user, &mut daftar_tukang_ledeng, &utils::UserRole::Tukang, &utils::AuthType::Register) {
-                        utils::MenuReturn::Lanjut => { continue 'dashboard_loop }
-                        utils::MenuReturn::Kembali => { continue 'auth_loop }
-                    }
-                }
-                else if  opsi_input == 3 { continue 'dashboard_loop }
-                else { println!("Opsi '{}' tidak tersedia\n", opsi_input) }
-            }
-        }
-        else if opsi_input == 3 {
-            println!("Bye");
-            break;
-        }
-        else { println!("Opsi '{}' tidak tersedia\n", opsi_input) }
-    }
+    utils::save_users_to_file(&daftar_user, "database/users.json").unwrap();
+    utils::save_tukang_to_file(&daftar_tukang_ledeng, "database/tukang_ledeng.json").unwrap();
+    utils::save_pesanan_to_file(&daftar_pesanan, "database/pesanan.json").unwrap();
 }
