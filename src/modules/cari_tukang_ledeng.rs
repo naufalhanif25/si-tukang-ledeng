abc47e875c3e12dea357e2106a4acc9a52b05e02
naufalhanif25 <naufal.hanif2525@gmail.com>
2025-11-25 00:30:05 +0700

    refactor: CariTukangLedeng class

--- src/modules/cari_tukang_ledeng.rs
+++ src/modules/cari_tukang_ledeng.rs
@@ -5,7 +5,15 @@ use crate::modules::enums::kategori::Kategori;
 pub struct CariTukangLedeng<'a> { sumber: &'a Vec<TukangLedeng> }
 
 impl<'a> CariTukangLedeng<'a> {
-    pub fn new(sumber: &'a Vec<TukangLedeng>) -> Self { return Self { sumber } }
-    pub fn cari(&self, kata_kunci: &str) -> Vec<&TukangLedeng> { return self.sumber.iter().filter(|item| item.nama.contains(kata_kunci) || item.lokasi.contains(kata_kunci)).collect() }
-    pub fn filter(&self, kategori: Kategori) -> Vec<&TukangLedeng> { return self.sumber.iter().filter(|item| *item.get_kategori() == kategori).collect() }
+    pub fn new(sumber: &'a Vec<TukangLedeng>) -> Self { 
+        return Self { sumber } 
+    }
+
+    pub fn cari(&self, kata_kunci: &str) -> Vec<&TukangLedeng> { 
+        return self.sumber.iter().filter(|item| item.nama.contains(kata_kunci) || item.lokasi.contains(kata_kunci)).collect() 
+    }
+
+    pub fn filter(&self, kategori: Kategori) -> Vec<&TukangLedeng> { 
+        return self.sumber.iter().filter(|item| *item.get_kategori() == kategori).collect() 
+    }
 }
