c625573f6bb7de094933c9e5eaacb7ab45acb3e6
naufalhanif25 <naufal.hanif2525@gmail.com>
2025-11-24 08:56:09 +0700

    feat: Add new methods

--- src/modules/enums/kategori.rs
+++ src/modules/enums/kategori.rs
@@ -1,4 +1,6 @@
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+use strum_macros::EnumIter;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, EnumIter)]
 pub enum Kategori {
     DomestikRumahTangga,
     Komersial,
@@ -10,4 +12,37 @@ pub enum Kategori {
     FasilitasPublik,
     GedungTinggi,
     KapalMaritim,
+}
+
+impl Kategori {
+    pub fn as_string(&self) -> &'static str {
+        match self {
+            Kategori::DomestikRumahTangga => "Domestik Rumah Tangga",
+            Kategori::Komersial => "Komersial",
+            Kategori::Industri => "Industri",
+            Kategori::ProyekKonstruksiBaru => "Proyek Konstruksi Baru",
+            Kategori::Renovasi => "Renovasi",
+            Kategori::PerawatanServisRutin => "Perawatan / Servis Rutin",
+            Kategori::Darurat => "Darurat",
+            Kategori::FasilitasPublik => "Fasilitas Publik",
+            Kategori::GedungTinggi => "Gedung Tinggi",
+            Kategori::KapalMaritim => "Kapal Maritim",
+        }
+    }
+
+    pub fn from_string(input: &str) -> Option<Kategori> {
+        match input {
+            "Domestik Rumah Tangga" => Some(Kategori::DomestikRumahTangga),
+            "Komersial" => Some(Kategori::Komersial),
+            "Industri" => Some(Kategori::Industri),
+            "Proyek Konstruksi Baru" => Some(Kategori::ProyekKonstruksiBaru),
+            "Renovasi" => Some(Kategori::Renovasi),
+            "Perawatan / Servis Rutin" => Some(Kategori::PerawatanServisRutin),
+            "Darurat" => Some(Kategori::Darurat),
+            "Fasilitas Publik" => Some(Kategori::FasilitasPublik),
+            "Gedung Tinggi" => Some(Kategori::GedungTinggi),
+            "Kapal Maritim" => Some(Kategori::KapalMaritim),
+            _ => None,
+        }
+    }
 }
