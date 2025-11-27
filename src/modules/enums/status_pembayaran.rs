1fbf5bbe1dfb0a9c7b4fdd83d4ecf0c5277e467a
naufalhanif25 <naufal.hanif2525@gmail.com>
2025-11-27 10:55:34 +0700

    chore: implement state pattern

--- src/modules/enums/status_pembayaran.rs
+++ src/modules/enums/status_pembayaran.rs
@@ -1,5 +1,6 @@
 use strum_macros::EnumIter;
 use serde::{ Serialize, Deserialize };
+use crate::modules::enums::status_state::*;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, EnumIter, Serialize, Deserialize)]
 pub enum StatusPembayaran {
@@ -9,6 +10,14 @@ pub enum StatusPembayaran {
 }
 
 impl StatusPembayaran {
+    pub fn to_state(&self) -> Box<dyn PaymentState> {
+        match self {
+            StatusPembayaran::Pending => Box::new(PendingState),
+            StatusPembayaran::Berhasil => Box::new(BerhasilState),
+            StatusPembayaran::Gagal => Box::new(GagalState),
+        }
+    }
+
     pub fn as_string(&self) -> &'static str {
         match self {
             StatusPembayaran::Berhasil => "Berhasil",
